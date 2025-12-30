008cbf47c7a2bcd557c1b724321d0bd6f7313ba5
Henrique <henriquesilveira700@gmail.com>
2025-12-30 11:43:47 -0300

    Add tips - variaveis

---
+++ lib/tips/variaveis.dart
@@ -0,0 +1,19 @@
+void main() {
+  var nome = 'Henrique';
+
+  final idade;
+  idade = 27;
+  const age = 16;
+  
+  var maiorIdade = true;
+
+  print(nome);
+
+  if (maiorIdade == false) {
+    print(idade);
+  } else { print(age);}
+
+
+  late String nomePai;
+  nomePai = 'Jose';
+} 
