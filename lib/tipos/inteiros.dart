5aea15fa1c6f570974e6ee5e87bb0eef88223481
Henrique <henriquesilveira700@gmail.com>
2026-01-07 11:26:45 -0300

    Inteiros

---
+++ lib/tipos/inteiros.dart
@@ -0,0 +1,14 @@
+void main(){
+  int altura = 177;
+  print('Altura: $altura cm');
+
+  int ano = 2025;
+  print('Ano: $ano');
+
+  int idade = 27;
+  print('Idade: $idade');
+
+  int anoNascimento = ano - idade;
+  print('Ano de Nascimento: $anoNascimento');
+
+}
