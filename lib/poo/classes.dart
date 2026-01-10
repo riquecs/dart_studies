c105fa5443c00e3fe9dbfe66d6cc8f9ba403ce50
Henrique <henriquesilveira700@gmail.com>
2026-01-10 17:19:56 -0300

    Poo - Classes

---
+++ lib/poo/classes.dart
@@ -0,0 +1,12 @@
+class Pessoa {
+  
+  final String nome;
+  final double peso;
+  final double altura; 
+  final int idade;
+
+  //Construtor this é por parametro posicional
+  Pessoa(this.nome, this.peso, this.altura, this.idade);
+}
+
+void main(){}
